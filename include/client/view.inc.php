6410fe89ceed8a0fe16c72a0d10f73a42f446edb
Anatoliy Kolodkin <anatoliy@ankosoftware.com>
2026-01-31 06:41:59 -0600

    Redesign ticket view with Jira-style modern UX
    Layout:
    - Two-column layout with main content and details sidebar
    - Sticky sidebar for ticket details
    - Breadcrumb navigation back to tickets list
    
    Header:
    - Ticket number as monospace badge
    - Prominent title display
    - Action buttons (Edit, Print) aligned right
    
    Reply Section:
    - Positioned at top of activity feed
    - Clean textarea with placeholder
    - Primary "Send" and secondary "Send & Close" buttons
    
    Activity Feed:
    - Card container with section header
    - Comments in descending order (newest first)
    - Blue left border for customer messages
    - Green left border for staff responses
    - Timeline-style layout with avatars
    
    Details Sidebar:
    - Status card with colored badge
    - Details card (Department, Created, Updated)
    - Reporter card with avatar initial
    - Custom fields in separate cards

--- include/client/view.inc.php
+++ include/client/view.inc.php
@@ -7,6 +7,8 @@ $type = array('type' => 'viewed');
 Signal::send('object.view', $ticket, $type);
 
 $dept = $ticket->getDept();
+$status = $ticket->getStatus();
+$statusState = $status ? $status->getState() : 'open';
 
 if ($ticket->isClosed() && !$ticket->isReopenable())
     $warn = sprintf(__('%s is marked as closed and cannot be reopened.'), __('This ticket'));
@@ -15,195 +17,208 @@ if ($ticket->isClosed() && !$ticket->isReopenable())
 if(!$dept || !$dept->isPublic())
     $dept = $cfg->getDefaultDept();
 
-if ($thisclient && $thisclient->isGuest()
-    && $cfg->isClientRegistrationEnabled()) { ?>
+if ($blockReply = $ticket->isChild() && $ticket->getMergeType() != 'visual')
+    $warn = sprintf(__('This Ticket is Merged into another Ticket. Please go to the %s%d%s to reply.'),
+        '<a href="tickets.php?id=', $ticket->getPid(), '" style="text-decoration:underline">Parent</a>');
 
-<div id="msg_info">
-    <i class="icon-compass icon-2x pull-left"></i>
-    <strong><?php echo __('Looking for your other tickets?'); ?></strong><br />
-    <a href="<?php echo ROOT_PATH; ?>login.php?e=<?php
-        echo urlencode($thisclient->getEmail());
-    ?>" style="text-decoration:underline"><?php echo __('Sign In'); ?></a>
-    <?php echo sprintf(__('or %s register for an account %s for the best experience on our help desk.'),
-        '<a href="account.php?do=create" style="text-decoration:underline">','</a>'); ?>
-    </div>
-
-<?php } ?>
-
-<table width="800" cellpadding="1" cellspacing="0" border="0" id="ticketInfo">
-    <tr>
-        <td colspan="2" width="100%">
-            <h1>
-                <a href="tickets.php?id=<?php echo $ticket->getId(); ?>" title="<?php echo __('Reload'); ?>"><i class="refresh icon-refresh"></i></a>
-                <b>
-                <?php $subject_field = TicketForm::getInstance()->getField('subject');
-                    echo $subject_field->display($ticket->getSubject()); ?>
-                </b>
-                <small>#<?php echo $ticket->getNumber(); ?></small>
-<div class="pull-right">
-      <a class="action-button" href="tickets.php?a=print&id=<?php
-          echo $ticket->getId(); ?>"><i class="icon-print"></i> <?php echo __('Print'); ?></a>
-
-<?php if ($ticket->hasClientEditableFields()
-        // Only ticket owners can edit the ticket details (and other forms)
-        && $thisclient->getId() == $ticket->getUserId()) { ?>
-                <a class="action-button" href="tickets.php?a=edit&id=<?php
-                     echo $ticket->getId(); ?>"><i class="icon-edit"></i> <?php echo __('Edit'); ?></a>
-<?php } ?>
-</div>
-            </h1>
-        </td>
-    </tr>
-    <tr>
-        <td width="50%">
-            <table class="infoTable" cellspacing="1" cellpadding="3" width="100%" border="0">
-                <thead>
-                    <tr><td class="headline" colspan="2">
-                        <?php echo __('Basic Ticket Information'); ?>
-                    </td></tr>
-                </thead>
-                <tr>
-                    <th width="100"><?php echo __('Ticket Status');?>:</th>
-                    <td><?php echo ($S = $ticket->getStatus()) ? $S->getLocalName() : ''; ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo __('Department');?>:</th>
-                    <td><?php echo Format::htmlchars($dept instanceof Dept ? $dept->getName() : ''); ?></td>
-                </tr>
-                <tr>
-                    <th><?php echo __('Create Date');?>:</th>
-                    <td><?php echo Format::datetime($ticket->getCreateDate()); ?></td>
-                </tr>
-           </table>
-       </td>
-       <td width="50%">
-           <table class="infoTable" cellspacing="1" cellpadding="3" width="100%" border="0">
-                <thead>
-                    <tr><td class="headline" colspan="2">
-                        <?php echo __('User Information'); ?>
-                    </td></tr>
-                </thead>
-               <tr>
-                   <th width="100"><?php echo __('Name');?>:</th>
-                   <td><?php echo mb_convert_case(Format::htmlchars($ticket->getName()), MB_CASE_TITLE); ?></td>
-               </tr>
-               <tr>
-                   <th width="100"><?php echo __('Email');?>:</th>
-                   <td><?php echo Format::htmlchars($ticket->getEmail()); ?></td>
-               </tr>
-               <tr>
-                   <th><?php echo __('Phone');?>:</th>
-                   <td><?php echo $ticket->getPhoneNumber(); ?></td>
-               </tr>
-            </table>
-       </td>
-    </tr>
-    <tr>
-        <td colspan="2">
-<!-- Custom Data -->
-<?php
-$sections = $forms = array();
+// Get custom fields data
+$customSections = $customForms = array();
 foreach (DynamicFormEntry::forTicket($ticket->getId()) as $i=>$form) {
-    // Skip core fields shown earlier in the ticket view
     $answers = $form->getAnswers()->exclude(Q::any(array(
         'field__flags__hasbit' => DynamicFormField::FLAG_EXT_STORED,
         'field__name__in' => array('subject', 'priority'),
         Q::not(array('field__flags__hasbit' => DynamicFormField::FLAG_CLIENT_VIEW)),
     )));
-    // Skip display of forms without any answers
     foreach ($answers as $j=>$a) {
         if ($v = $a->display())
-            $sections[$i][$j] = array($v, $a);
+            $customSections[$i][$j] = array($v, $a);
     }
-    // Set form titles
-    $forms[$i] = $form->getTitle();
+    $customForms[$i] = $form->getTitle();
 }
-foreach ($sections as $i=>$answers) {
-    ?>
-        <table class="custom-data" cellspacing="0" cellpadding="4" width="100%" border="0">
-        <tr><td colspan="2" class="headline flush-left"><?php echo $forms[$i]; ?></th></tr>
-<?php foreach ($answers as $A) {
-    list($v, $a) = $A; ?>
-        <tr>
-            <th><?php
-echo $a->getField()->get('label');
-            ?>:</th>
-            <td><?php
-echo $v;
-            ?></td>
-        </tr>
+
+$subject_field = TicketForm::getInstance()->getField('subject');
+?>
+
+<?php if ($thisclient && $thisclient->isGuest() && $cfg->isClientRegistrationEnabled()) { ?>
+<div class="guest-banner">
+    <i class="icon-info-circle"></i>
+    <span><?php echo __('Looking for your other tickets?'); ?>
+    <a href="<?php echo ROOT_PATH; ?>login.php?e=<?php echo urlencode($thisclient->getEmail()); ?>"><?php echo __('Sign In'); ?></a>
+    <?php echo __('or'); ?>
+    <a href="account.php?do=create"><?php echo __('register for an account'); ?></a></span>
+</div>
 <?php } ?>
-        </table>
-    <?php
-} ?>
-    </td>
-</tr>
-</table>
-<br>
-  <?php
-    $email = $thisclient->getUserName();
-    $clientId = TicketUser::lookupByEmail($email)->getId();
 
-    $ticket->getThread()->render(array('M', 'R', 'user_id' => $clientId), array(
-                    'mode' => Thread::MODE_CLIENT,
-                    'html-id' => 'ticketThread')
-                );
-if ($blockReply = $ticket->isChild() && $ticket->getMergeType() != 'visual')
-    $warn = sprintf(__('This Ticket is Merged into another Ticket. Please go to the %s%d%s to reply.'),
-        '<a href="tickets.php?id=', $ticket->getPid(), '" style="text-decoration:underline">Parent</a>');
-  ?>
+<!-- Jira-style Ticket Header -->
+<div class="ticket-header">
+    <div class="ticket-breadcrumb">
+        <a href="tickets.php"><i class="icon-arrow-left"></i> <?php echo __('Back to Tickets'); ?></a>
+    </div>
+    <div class="ticket-title-row">
+        <div class="ticket-title">
+            <span class="ticket-key"><?php echo $ticket->getNumber(); ?></span>
+            <h1><?php echo $subject_field->display($ticket->getSubject()); ?></h1>
+        </div>
+        <div class="ticket-actions">
+<?php if ($ticket->hasClientEditableFields() && $thisclient->getId() == $ticket->getUserId()) { ?>
+            <a class="btn-action" href="tickets.php?a=edit&id=<?php echo $ticket->getId(); ?>">
+                <i class="icon-edit"></i> <?php echo __('Edit'); ?>
+            </a>
+<?php } ?>
+            <a class="btn-action" href="tickets.php?a=print&id=<?php echo $ticket->getId(); ?>">
+                <i class="icon-print"></i> <?php echo __('Print'); ?>
+            </a>
+        </div>
+    </div>
+</div>
 
-<div class="clear" style="padding-bottom:10px;"></div>
 <?php if($errors['err']) { ?>
     <div id="msg_error"><?php echo $errors['err']; ?></div>
 <?php }elseif($msg) { ?>
     <div id="msg_notice"><?php echo $msg; ?></div>
 <?php }elseif($warn) { ?>
     <div id="msg_warning"><?php echo $warn; ?></div>
-<?php }
-if ((!$ticket->isClosed() || $ticket->isReopenable()) && !$blockReply) { ?>
-<form id="reply" action="tickets.php?id=<?php echo $ticket->getId();
-?>#reply" name="reply" method="post" enctype="multipart/form-data">
-    <?php csrf_token(); ?>
-    <h2><?php echo __('Post a Reply');?></h2>
-    <input type="hidden" name="id" value="<?php echo $ticket->getId(); ?>">
-    <input type="hidden" name="a" value="reply">
-    <div>
-        <p><em><?php
-         echo __('To best assist you, we request that you be specific and detailed'); ?></em>
-        <font class="error">*&nbsp;<?php echo $errors['message']; ?></font>
-        </p>
-        <textarea name="<?php echo $messageField->getFormName(); ?>" id="message" cols="50" rows="9" wrap="soft"
-            class="<?php if ($cfg->isRichTextEnabled()) echo 'richtext';
-                ?> draft" <?php
-list($draft, $attrs) = Draft::getDraftAndDataAttrs('ticket.client', $ticket->getId(), $info['message']);
-echo $attrs; ?>><?php echo $draft ?: $info['message'];
-            ?></textarea>
-    <?php
-    if ($messageField->isAttachmentsEnabled()) {
-        print $attachments->render(array('client'=>true));
-    } ?>
-    </div>
-<?php
-  if ($ticket->isClosed() && $ticket->isReopenable()) { ?>
-    <div class="warning-banner">
-        <?php echo __('Ticket will be reopened on message post'); ?>
-    </div>
 <?php } ?>
-    <p style="text-align:center">
-        <input type="submit" value="<?php echo __('Post Reply');?>">
-<?php if ($cfg->allowClientClose()
-        && !$ticket->isClosed()
-        && $thisclient->getId() == $ticket->getUserId()) { ?>
-        <input type="submit" name="close_on_reply" value="<?php echo __('Post Reply and Close');?>"
-            onclick="return confirm('<?php echo __('Are you sure you want to close this ticket?'); ?>');">
+
+<!-- Jira-style Two Column Layout -->
+<div class="ticket-layout">
+    <!-- Main Content Area -->
+    <div class="ticket-main">
+        <!-- Reply Form -->
+<?php if ((!$ticket->isClosed() || $ticket->isReopenable()) && !$blockReply) { ?>
+        <div class="reply-section">
+            <form id="reply" action="tickets.php?id=<?php echo $ticket->getId(); ?>#reply" name="reply" method="post" enctype="multipart/form-data">
+                <?php csrf_token(); ?>
+                <input type="hidden" name="id" value="<?php echo $ticket->getId(); ?>">
+                <input type="hidden" name="a" value="reply">
+                <div class="reply-input-wrapper">
+                    <textarea name="<?php echo $messageField->getFormName(); ?>" id="message"
+                        placeholder="<?php echo __('Add a comment...'); ?>"
+                        class="<?php if ($cfg->isRichTextEnabled()) echo 'richtext'; ?> draft" <?php
+                        list($draft, $attrs) = Draft::getDraftAndDataAttrs('ticket.client', $ticket->getId(), $info['message']);
+                        echo $attrs; ?>><?php echo $draft ?: $info['message']; ?></textarea>
+                    <?php if ($messageField->isAttachmentsEnabled()) {
+                        print $attachments->render(array('client'=>true));
+                    } ?>
+                </div>
+<?php if ($ticket->isClosed() && $ticket->isReopenable()) { ?>
+                <div class="reopen-notice">
+                    <i class="icon-info-circle"></i> <?php echo __('Ticket will be reopened on message post'); ?>
+                </div>
 <?php } ?>
-        <input type="reset" value="<?php echo __('Reset');?>">
-        <input type="button" value="<?php echo __('Cancel');?>" onClick="history.go(-1)">
-    </p>
-</form>
+                <div class="reply-actions">
+                    <button type="submit" class="btn-primary">
+                        <i class="icon-paper-plane"></i> <?php echo __('Send'); ?>
+                    </button>
+<?php if ($cfg->allowClientClose() && !$ticket->isClosed() && $thisclient->getId() == $ticket->getUserId()) { ?>
+                    <button type="submit" name="close_on_reply" class="btn-secondary"
+                        onclick="return confirm('<?php echo __('Are you sure you want to close this ticket?'); ?>');">
+                        <i class="icon-check"></i> <?php echo __('Send & Close'); ?>
+                    </button>
+<?php } ?>
+                </div>
+                <?php if ($errors['message']) { ?>
+                <div class="field-error"><?php echo $errors['message']; ?></div>
+                <?php } ?>
+            </form>
+        </div>
+<?php } ?>
+
+        <!-- Activity Section -->
+        <div class="activity-section">
+            <div class="activity-header">
+                <h3><i class="icon-comments"></i> <?php echo __('Activity'); ?></h3>
+                <span class="activity-count"><?php
+                    $email = $thisclient->getUserName();
+                    $clientId = TicketUser::lookupByEmail($email)->getId();
+                ?></span>
+            </div>
+            <div class="activity-feed">
 <?php
-} ?>
+    $ticket->getThread()->render(array('M', 'R', 'user_id' => $clientId), array(
+                    'mode' => Thread::MODE_CLIENT,
+                    'html-id' => 'ticketThread',
+                    'sort' => 'DESC')
+                );
+?>
+            </div>
+        </div>
+    </div>
+
+    <!-- Details Sidebar -->
+    <div class="ticket-sidebar">
+        <!-- Status Card -->
+        <div class="detail-card">
+            <div class="detail-card-header">
+                <h4><?php echo __('Status'); ?></h4>
+            </div>
+            <div class="detail-card-body">
+                <span class="status-badge status-<?php echo $statusState; ?>">
+                    <?php echo $status ? $status->getLocalName() : __('Open'); ?>
+                </span>
+            </div>
+        </div>
+
+        <!-- Details Card -->
+        <div class="detail-card">
+            <div class="detail-card-header">
+                <h4><?php echo __('Details'); ?></h4>
+            </div>
+            <div class="detail-card-body">
+                <div class="detail-row">
+                    <span class="detail-label"><?php echo __('Department'); ?></span>
+                    <span class="detail-value"><?php echo Format::htmlchars($dept instanceof Dept ? $dept->getName() : ''); ?></span>
+                </div>
+                <div class="detail-row">
+                    <span class="detail-label"><?php echo __('Created'); ?></span>
+                    <span class="detail-value"><?php echo Format::datetime($ticket->getCreateDate()); ?></span>
+                </div>
+                <div class="detail-row">
+                    <span class="detail-label"><?php echo __('Updated'); ?></span>
+                    <span class="detail-value"><?php echo Format::datetime($ticket->getLastMsgDate() ?: $ticket->getCreateDate()); ?></span>
+                </div>
+            </div>
+        </div>
+
+        <!-- Reporter Card -->
+        <div class="detail-card">
+            <div class="detail-card-header">
+                <h4><?php echo __('Reporter'); ?></h4>
+            </div>
+            <div class="detail-card-body">
+                <div class="reporter-info">
+                    <div class="reporter-avatar">
+                        <?php echo mb_strtoupper(mb_substr($ticket->getName()->getFirst(), 0, 1)); ?>
+                    </div>
+                    <div class="reporter-details">
+                        <div class="reporter-name"><?php echo Format::htmlchars($ticket->getName()); ?></div>
+                        <div class="reporter-email"><?php echo Format::htmlchars($ticket->getEmail()); ?></div>
+                        <?php if ($ticket->getPhoneNumber()) { ?>
+                        <div class="reporter-phone"><?php echo $ticket->getPhoneNumber(); ?></div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Custom Fields -->
+<?php foreach ($customSections as $i=>$answers) { ?>
+        <div class="detail-card">
+            <div class="detail-card-header">
+                <h4><?php echo $customForms[$i]; ?></h4>
+            </div>
+            <div class="detail-card-body">
+<?php foreach ($answers as $A) { list($v, $a) = $A; ?>
+                <div class="detail-row">
+                    <span class="detail-label"><?php echo $a->getField()->get('label'); ?></span>
+                    <span class="detail-value"><?php echo $v; ?></span>
+                </div>
+<?php } ?>
+            </div>
+        </div>
+<?php } ?>
+    </div>
+</div>
 <script type="text/javascript">
 <?php
 // Hover support for all inline images
